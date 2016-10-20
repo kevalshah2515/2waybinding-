d66d6d46238de63cce9dc96cdc986342a84eeccf
kevalshah2515 <kevalshah2515@gmail.com>
2016-10-21 00:00:33 +0530

    Add files via upload

---
+++ 2way binding AngJs/2waybind.js
@@ -0,0 +1,17 @@
+var app=angular.module("app",[]);
+		app.controller("myController",['$scope',function($scope){
+			$scope.student={
+				id:1,
+				name:'keval',
+				cgpa:5
+			};
+
+
+
+
+
+		}
+
+
+			]);
+	
